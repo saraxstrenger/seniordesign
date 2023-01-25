49abe798c1cd3cc20508077e75b135f09da417ef
Saurabh Shah <saurabh111233212@gmail.com>
2023-01-25 13:52:33 -0500

    Refactored and fixed spelling lol

--- RecSystem/EmbeddingRecomender.py
+++ RecSystem/EmbeddingRecomender.py
@@ -1,13 +1,7 @@
-#pip install --upgrade torch
-#pip install transformers
 #pip install sentence_transformers
-import torch
-from transformers import BertModel, BertTokenizer
 import numpy as np
 import heapq
 from sentence_transformers import SentenceTransformer
-
-
 class EmbeddingRecomender():
     # takes in an iterable course_names, where each elt is a course name (string)
     def __init__(self, course_names):
@@ -20,10 +14,6 @@ class EmbeddingRecomender():
             course_embedding = self.embed(course_name)
             self.courses.append((course_name, course_embedding))
     
-    def embed(self, text):
-        embedding = self.model.encode(text)
-        return embedding
-    
     # returns the BERT emebdding of text
     # Q for future Surb: is it easy to run BERT on the cloud from say, a lambda call? 
     # is the spinup gonna be atrocious for that or is huggingface the goat?
@@ -32,30 +22,18 @@ class EmbeddingRecomender():
     # collabortive filtering to predict tabular values would be sick 
     # Imagine a system that says "I think X course will be 3.5 difficulty for you"
     # No need to adjust for things "Well I'm good at coding and bad at theroy so I should add 0.5 to this diff score..."
-    
-    """
-    def embed_old(self, text):
-        # Encode the input sentence
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.model = BertModel.from_pretrained('bert-base-uncased')
+    def embed(self, text):
+        embedding = self.model.encode(text)
+        return embedding
         
-        input_ids = torch.tensor(self.tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)
-        outputs = self.model(input_ids)
-        # since we're embedding "entire sentences", just grab the contextual embedding of the CLS token
-        CLS_sentence_embedding = outputs[0][0][0]
-        
-        return CLS_sentence_embedding.detach().numpy()
-    """
-    #^also oops my bad for trying to do the sentence embeddings myself instead of just using the huggingface lib for it
-    #^kinda worked but not really
-
     # returns the k nearest neighbors to target from candidates, using euclidian distance 
     # Q for future Saurabh: Is Euclidian distance a wise distance measure here? 
     # Aren't the top few principal components wayy more important than the rest?
-    # or is language complex enough as for all 300 principal components to be important (and roughly equally important)?
+    # or is natural language complex enough as for all 300 principal components to be important (and roughly equally important)?
     def k_nearest_neighbors(self, target, k):
         heap = []
         tgt_embedding = self.embed(target)
+        
         for (course_name, course_embedding) in self.courses:
             diff = course_embedding - tgt_embedding
             dist = np.sqrt(np.dot(diff.T, diff))
@@ -65,34 +43,40 @@ class EmbeddingRecomender():
                 heapq.heappop(heap)
         
         heap.sort(key= lambda x: -x[0])
-        #print(heap)
         
         top_k = []
         for (_, course_name) in heap:
             top_k.append(course_name)
         return top_k
 
-    # returns some reccomendations from class_titles which are semantically closest to job_title
+    # returns 3 reccomendations from class_titles which are semantically closest to job_title
     def emebedding_rec(self, interest):
         return self.k_nearest_neighbors(interest, 3)
 
-rec = EmbeddingRecomender([ 'Software Engineering', 'Introduction to Acting', 'Creative Writing: Extreme Noticing', 
+    # prints reccomendations onto console (for light testing/debugging)
+    def print_rec(self, interest):
+        print(interest + ': ' + str(self.emebedding_rec(interest)))
+    
+    
+def main():
+    rec = EmbeddingRecomender([ 'Software Engineering', 'Introduction to Acting', 'Creative Writing: Extreme Noticing', 
                             'Computer Architecture', 'Data Structures and Algorithms', 'Dynamic Systems',
                             'Machine Learning', 'Deep Learning', 'Artificial Intelligence',
                             'Introduction to Drawing', 'Advanced Drawing', 'US History 1950 to Present',
                             'Programming and Problem Solving', 'Computer Graphics', 'Human Systems Engineering',
-                            'Building Buidlings', 'How to Make Bridges', 'Mechanics', 'Elctromagnetism',
-                            'Intro to Product Design'])
-
-
-def test_rec(rec, interest):
-    print(interest + ': ' + str(rec.emebedding_rec(interest)))
+                            'Architecture 101: Building Buidlings', 'How to Make Bridges', 'Mechanics', 'Electromagnetism',
+                            'Intro to Product Design', 'User Experience Research', 'Woodworking 101',
+                            'How to make things'])
     
-    
-test_rec(rec, 'Software Engineer')
-test_rec(rec, 'Painter')
-test_rec(rec, 'Historian')
-test_rec(rec, 'Architect')
-test_rec(rec, 'Phycicist')
-test_rec(rec, 'Product Manager')
-test_rec(rec, 'Machine Learning Engineer')
+    rec.print_rec('Software Engineer')
+    rec.print_rec('Painter')
+    rec.print_rec('Historian')
+    rec.print_rec('Architect')
+    rec.print_rec('Physicist')
+    rec.print_rec('Product Manager')
+    rec.print_rec('Machine Learning Engineer')
+    rec.print_rec('I\'d like to work with customers to figure out their wants for software products')
+    rec.print_rec('I like using my hands to build physical things')
+
+if __name__ == '__main__':
+    main()
