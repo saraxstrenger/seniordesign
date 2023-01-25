1ebffcdbbfa143201269ca3af1e453537694ce56
Sara Strenger <saraxst@seas.upenn.edu>
2023-01-24 20:04:39 -0500

    linked client and server with some dummy test code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState(null);
+  const [info, setInfo]= React.useState({name: "Janavi", year: 2023});
+  React.useEffect(() => {
+    fetch("/home")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
+        <p> {info.name}</p>
+        <p> {info.year?.toString()}</p>
+        <button onClick={()=> setInfo({name: "sara"})}>button</button>
+      
       </header>
     </div>
   );
 }
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
+
+app.get("/home", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
