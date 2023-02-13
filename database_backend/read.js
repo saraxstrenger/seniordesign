d6056166e0ffa784da4283243a7f941b9585ecf9
Suvas Kota <suvaskota@brb-15629.apn.wlan.private.upenn.edu>
2023-02-13 17:03:55 -0500

    initial database commit

---
+++ database_backend/read.js
@@ -0,0 +1,28 @@
+var AWS = require("aws-sdk");
+let awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": "XXXXXXX", "secretAccessKey": "XXXXXXXX"
+};
+AWS.config.update(awsConfig);
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+let fetchOneByKey = function () {
+    var params = {
+        TableName: "User_Table", // Change this to the table to you want to read from
+        Key: {
+            "Username": "s400" // Change this to the key of the item you want to read
+        }
+    };
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+        }
+        else {
+            console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+        }
+    })
+}
+
+
+fetchOneByKey();
