41ba440c60699ac6e0e0baca7fca46c3239159b3
Sara Strenger <saraxst@seas.upenn.edu>
2023-04-03 10:53:58 -0400

    modified rec script to update recs for a user only when new interests are present, instead of taking interest as an argument

--- RecSystem/Database.py
+++ RecSystem/Database.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 from EmbeddingRecommender import EmbeddingRecommender
 import argparse
 # uploads the emebddings to the database
@@ -15,30 +16,68 @@ def upload_emebddings(course_table, embed_rec):
       )
 
 
-def update_rec(username, interest, num_recs, embed_rec, user_table):
-  recs = embed_rec.k_nearest_neighbors(interest, num_recs)
-  course_codes = [name[:8] for name in recs]
+def update_rec(username, num_recs, embed_rec, user_table):
+  try:
+    response = user_table.get_item(
+      Key={
+        'username': username
+      },
+    )
+  except ClientError as err:
+    print(
+        "Couldn't get user %s from user table. Here's why: %s: %s",
+        username,
+        err.response['Error']['Code'], 
+        err.response['Error']['Message'])
+    return
   
-  response = user_table.get_item(
-    Key={
-      'username': username
-    },
-  )
-  
+  existing_interests = []
+  if "interests" in response["Item"]:
+    existing_interests = response["Item"]["interests"]
+
   exististing_recs = {}
   if 'recs' in response['Item']:
     exististing_recs = response['Item']['recs']
   
-  exististing_recs[interest] = course_codes
+  new_interests =[]
+  for interest in existing_interests:
+    if interest not in exististing_recs:
+      new_interests.append(interest)
+
+  # terminate early if no computation should be made
+  if new_interests==0:
+    return
+
+  # compute new recommendations
+  new_recs = {}
+  for interest in new_interests:
+    recs = embed_rec.k_nearest_neighbors(interest, num_recs)
+    course_codes = [name[:8] for name in recs]
+    new_recs[interest] = course_codes
   
+  # build update expression such that we only add new interests
+  update_expression =  'SET '
+  attribute_names={}
+  attribute_values={}
+  index=0
+  for interest, recs in new_recs.items():
+    if index != 0:
+      update_expression += ', '
+    attribute = '#interst'+str(index)
+    attribute_names[attribute] = interest
+    value = ':val'+str(index)
+    attribute_values[value] = recs
+
+    update_expression += "recs."+attribute + ' = '+value
+    index += 1
+
   user_table.update_item(
     Key={
       'username': username
     },
-    UpdateExpression='SET recs = :val1',
-    ExpressionAttributeValues={
-      ':val1': exististing_recs
-    }
+    UpdateExpression=update_expression,
+    ExpressionAttributeNames=attribute_names,
+    ExpressionAttributeValues=attribute_values,
   )
   
   
@@ -52,16 +91,12 @@ def main():
   user_table = dynamodb.Table(user_table_name)
   parser = argparse.ArgumentParser()
   parser.add_argument('-u', '--username', type=str)
-  parser.add_argument('-i', '--interest', type=str)
   parser.add_argument('-n', '--num_recs', type=int)
   
   args = parser.parse_args()
-  interest = args.interest
   num_recs = args.num_recs
   username = args.username
-    
-  
-  update_rec(username, interest, num_recs, embed_rec, user_table)
+  update_rec(username, num_recs, embed_rec, user_table)
   
   
 if __name__ == '__main__':
