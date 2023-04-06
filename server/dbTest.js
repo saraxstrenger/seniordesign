b79d19992a6db0377ea98879ad506824c2169346
skrockz <skota@seas.upenn.edu>
2023-04-06 00:30:22 -0400

    tested updateEval and deleteEval, and they work!
    dbTest.js calls some of the functions from database.js for the purpose of testing

--- server/dbTest.js
+++ server/dbTest.js
@@ -1,5 +1,42 @@
-// import * as db from "./database.js";
-import * as req from "./endpoints.js";
+import * as db from "./database.js";
+//import * as req from "./endpoints.js";
 
 
-req.sendSocketMessage("sara");
+//req.sendSocketMessage("sara");
+
+
+// const params = {username: "skota", password: "pass", first: "Suvas", last: "Kota", email: "skota@seas.upenn.edu", entranceYear: "2019", major: "CIS"};
+// db.createUser(params, (err, data) => {
+//     if (err) {
+//       console.log("Error", err.stack);
+//     } else {
+//       console.log(data);
+//     }
+//   });
+
+// const params = { department: "CIS", number: "1210", year: "2020", semester: "Spring", difficulty: "3.5", interest: "4", workload:[4,4,4,4]};
+// db.addEvaluation("skota", params, (err, data) => {
+//     if (err) {
+//         console.log("Error", err.stack);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// const params = { department: "CIS", number: "1600", year: "2019", semester: "Fall", difficulty: "3.7", interest: "3.8", workload:[2,4,4,3]};
+// db.updateEvaluation("skota", params, (err, data) => {
+//     if (err) {
+//         console.log("Error", err.stack);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// const params = { department: "CIS", number: "1210", year: "2020", semester: "Spring"};
+// db.deleteEvaluation("skota", params, (err, data) => {
+//     if (err) {
+//         console.log("Error", err.stack);
+//     } else {
+//         console.log(data);
+//     }
+// });
